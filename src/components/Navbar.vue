59ade3a79c4e8416bb655add6394fcfea23d8c24
Daniel Ertl <dertl@web.de>
2022-12-22 16:24:37 +0100

    initial

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="navbar">
+        <div class="navLink">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.navbar{
+    background: #f44336;
+    padding: 1rem;
+    font-size: 1.5rem;
+    border-bottom: 1px solid white;
+
+
+}
+.navLink{
+text-align: center;
+}
+a{
+    text-decoration: none;
+    padding: 1rem;
+    color:#fff;
+    text-align: center;
+}
+@media only screen and (max-width: 600px) {
+    .navLink{
+        display: flex;
+        flex-direction: column;
+    }
+}
+</style>
