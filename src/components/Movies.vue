909ca7c4549d8391e1318175e36b90a2406a5746
Daniel Ertl <dertl@web.de>
2023-08-26 19:08:31 +0200

    Versuch einer Tabelle

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="section">
     <div class="title">Filme</div>
+    <div>
+      <b-table striped hover :items="items" :fields="fields"></b-table>
+    </div>
   </div>
 </template>
    
 <script>
   export default {
-  // eslint-disable-next-line
+      // eslint-disable-next-line
     name: 'Movies',
     components: {},
     props: {
       msg: String
+    },
+    data() {
+      return {
+        // Note 'isActive' is left out and will not appear in the rendered table
+        fields: [
+          {
+            key: 'last_name',
+            sortable: true
+          },
+          {
+            key: 'first_name',
+            sortable: false
+          },
+          {
+            key: 'age',
+            label: 'Person age',
+            sortable: true,
+            // Variant applies to the whole column, including the header and footer
+            variant: 'danger'
+          }
+        ],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
     }
   }
 </script>
