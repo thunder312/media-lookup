08d638b01f2b58fa6b729f775e08332a213882ba
Daniel Ertl <dertl@web.de>
2023-12-13 15:05:47 +0100

    creating toolbar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="toolbar">
+    Do something
+  </div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line
+  name: 'ToolBar',
+  components: {},
+  props: {
+    msg: String
+  }
+}
+</script>
