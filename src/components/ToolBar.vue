92d7b1273838f4369ed0a612f2486fce1c6720bd
Daniel Ertl <dertl@web.de>
2023-12-13 17:00:11 +0100

    toolbat with modal for a first test

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,13 +1,29 @@
 <template>
 <div class="toolbar">
-    Do something
+  <button class="button" @click="showModal = true" :hidden="showModal">Neu</button>
+			<transition name="modal">
+				<div class="modal-mask" v-if="showModal">
+					<NewModal @close="closeModal"/>
+				</div>
+			</transition>
   </div>
 </template>
 <script>
+import NewModal from './NewModal.vue';
 export default {
     // eslint-disable-next-line
   name: 'ToolBar',
-  components: {},
+	components: {NewModal},
+	data:function (){
+		return{
+			showModal: false
+		}
+	},
+	methods:{
+		closeModal(){
+			this.showModal = false;
+		}
+	},
   props: {
     msg: String
   }
