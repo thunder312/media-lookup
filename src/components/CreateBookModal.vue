d2544b45c7eef6e9812d658611ecfaef760bcc7b
Daniel Ertl <dertl@web.de>
2023-12-13 21:38:08 +0100

    adding "createNew" modals

---
+++ src/components/CreateBookModal.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="form-group">
+					<label>Buch Titel</label>
+					<input
+						type="text"
+						id="title"
+						class="form-control"
+						v-model="dataObj.title"
+					/>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-success" @click="handleSave">
+					Save
+				</button>
+				<button class="btn btn-secondary" @click="handleClose">
+					Close
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CreateBookModal",
+	data: function (){
+		return{
+			dataObj:{
+				title: '',
+				body:''
+			}
+		}
+	},
+	methods:{
+		handleSave(){
+      console.log(this.dataObj);
+      this.handleClose();
+		},
+		handleClose(){
+			this.$emit('close');
+		}
+	}
+}
+</script>
