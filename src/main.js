43f6d2d51323deb2c1aed814c251a56312883dc5
Daniel Ertl <dertl@web.de>
2023-12-14 18:40:13 +0100

    adding google API for Books API

--- src/main.js
+++ src/main.js
@@ -22,4 +22,10 @@ const emitter = mitt();
 const app = createApp(App);
 
 app.config.globalProperties.emitter = emitter;
-app.use(router).mount('#app');
+app.use(router)
+.use(GoogleAPIs, {
+  clientId: '1093010721849-m6ll2h1glpuj9jqkdo3cj57te08o90uk.apps.googleusercontent.com',
+  discoveryDocs: ['https://www.googleapis.com/books/v1/volumes'],
+  scope: 'https://www.googleapis.com/auth/books.readonly',
+})
+.mount('#app');
