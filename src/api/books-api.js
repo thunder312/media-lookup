a7cbd36f20f76ab79a6fbcac8d91e0ba24bed6de
Daniel Ertl <dertl@web.de>
2023-12-15 23:34:44 +0100

    books-api tester is working now.

---
+++ src/api/books-api.js
@@ -0,0 +1,47 @@
+const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+//const BASE_URL_ID = "https://www.googleapis.com/books/v1/volumes/";
+const maxResults = "maxResults=";
+const orderBy = "orderBy=";
+const printType = "printType=";
+
+export async function globalSearch(query) {
+    console.log("globalSearch(" + query + ")");
+    return new Promise((resolve, reject) => {
+        fetch(BASE_URL +
+                query +
+                "&" +
+                maxResults +
+                "40&" +
+                orderBy +
+                "relevance&" +
+                printType +
+                "books"
+                )
+            .then((response) => {
+                resolve(response.json());
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
+    });
+}
+
+export async function searchByIsbn(query) {
+    return new Promise((resolve, reject) => {
+        fetch(BASE_URL + "isbn:" +
+                query +
+                "&" +
+                maxResults +
+                "40&" +
+                orderBy +
+                "relevance&" +
+                printType +
+                "books")
+            .then((response) => {
+                resolve(response.json());
+            })
+            .catch((err) => {
+                reject(err.message);
+            });
+    });
+}
--- src/components/GoogleApiTester.vue
+++ src/components/GoogleApiTester.vue
@@ -1,19 +1,67 @@
 <template>
     <GoogleLogin :callback="this.callback"/>
+    <h3>Look for book</h3>
+    <table>
+        <tr>
+          <td>
+            Buchtitel
+          </td>
+          <td>
+            <input @keyup.enter="search" v-model="this.inputTitle" id="booktitle-input" type="text" placeholder="Buchtitel"/>
+          </td>
+          <td>Sophies Welt</td>
+        </tr>
+        <tr>
+          <td>
+            ISBN
+          </td>
+          <td>
+            <input @keyup.enter="searchIsbn" v-model="this.inputIsbn" id="isbn-input" type="text" placeholder="ISBN"/>
+          </td>
+          <td>9783862222391</td>
+        </tr>   
+    </table>
+    <div> 
+      <pre v-if="books != null">
+        <div>Ergebnisse insgesamt: {{ books.length }}</div>
+
+        <table class="resultTable">
+          <tr>
+            <th></th>
+            <th></th>
+          </tr>
+          <tr>
+            <td class="category">Titel:</td>
+            <td>{{ books[0].volumeInfo.title }}</td>
+          </tr>
+          <tr>
+            <td class="category">Unter-Titel:</td>
+            <td>{{ books[0].volumeInfo.subtitle }}</td>
+          </tr>
+          <tr>
+            <td class="category">Autor:</td>
+            <td>{{ books[0].volumeInfo.authors }}</td>
+          </tr>
+        </table>
+
+        <!--{{ JSON.stringify(this.books, null, 2) }}-->
+      </pre>
+    </div>
 </template>
 
 <script>
+import { globalSearch } from "../api/books-api.js";
+import { searchByIsbn } from "../api/books-api.js";
+
 export default {
   name: 'GoogleApiTester',
-  props: {
-    msg: String
-  },
+  components: { },
   data () {
     return {
-      isSignedIn: false,
-      user: null,
-      userName: '',
       callback: null,
+      books: null,
+      inputTitle: "",
+      inputIsbn: "",
     }
   },
   created ()  { 
@@ -23,8 +71,37 @@ export default {
       console.log("Handle the response", response)
     }
   },
+  mounted() {},
   methods: {
-    
-  }
+    async search() {
+      await globalSearch(this.inputTitle).then((response) => {
+        if (response.items) {
+          this.books = response.items;
+        } else {
+          this.books = {};
+          this.noResults = true;
+        }
+      });
+    },
+    async searchIsbn() {
+      await searchByIsbn(this.inputIsbn).then((response) => {
+        if (response.items) {
+          this.books = response.items;
+        } else {
+          this.books = {};
+          this.noResults = true;
+        }
+      });
+    },
+  },
 }
 </script>
+<style>
+table.resultTable {
+  .category {
+    width: 150px;
+    font-weight: bold;
+    font-size: 20px;
+  }
+}
+</style>
